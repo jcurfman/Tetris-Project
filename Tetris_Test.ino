d217bdab41ffebfffb5ea3c434136e5f6e0d62b3
Joshua Curfman <joshua.curfman@tecnovelty.com>
2020-03-30 16:31:59 -0700

    Implemented reset capability
    Very broken, but implemented a software/hardware reset using a jumper between pin D12 and RESET.

--- Tetris_Test.ino
+++ Tetris_Test.ino
@@ -4,23 +4,28 @@
 int boardArray[200]; //Standard Tetris field is a 10x20 column
 int ActivePiece[4]; //Stores pixel locations of active (dropping) Tetromino
 
+#define resetPin 12 //Jumper from pin 12 to RESET pin
 #define NumPieces 7
 
 void setup() {
   //Begin serial output. Prints an empty field and generates a block to start.
   Serial.begin(9600);
+  pinMode(resetPin, INPUT);
+  digitalWrite(resetPin, LOW);
+  Serial.println("");
   Serial.println("Begin Tetris Test");
   SerialPrintGame();
-  GenerateNewBlock();
+  NewBlock();
 }
 
 void loop() {
   //Base ongoing logic of the game, run once per frame. 
   SerialPrintGame();
-  ActiveBlockDown();
+  MoveDown();
+  //ActiveBlockDown();
   Serial.println("");
   //delay is based loosely off speed of level 2 on NES Tetris.
-  delay(800); 
+  delay(800); //800 is playing default?
 }
 
 void SerialPrintGame() {
@@ -37,7 +42,7 @@ void SerialPrintGame() {
 }
 
 void MoveDown() {
-  //Deeply flawed, but functional. Moves all floating pieces down
+  //Deeply flawed. Moves all floating pieces down
   for(int i=(sizeof(boardArray)/sizeof(boardArray[0]))-1; i>=0; i--) {
     if(boardArray[i]==0 && boardArray[i-10]!=0) {
       if(i<10) {
@@ -46,6 +51,10 @@ void MoveDown() {
       boardArray[i]=boardArray[i-10];
       boardArray[i-10]=0;
     }
+    else if(boardArray[i]!=0) {
+      NewBlock();
+      Serial.println("New Block");
+    }
   }
 }
 
@@ -53,6 +62,7 @@ void ActiveBlockDown() {
   //Runs a collision check, then moves the active piece down if allowed
   bool stopBlock=CollisionCheck();
   if(stopBlock==false) {
+    //Collision Check very broken
     for(int i=0; i<4; i++) {
       int val=boardArray[ActivePiece[i]];
       boardArray[ActivePiece[i]]=0;
@@ -62,7 +72,7 @@ void ActiveBlockDown() {
   }
   //Generates a new block if no further motion is allowed
   else if(stopBlock==true) {
-    GenerateNewBlock();
+    NewBlock();
     Serial.println("New Block");
   }
 }
@@ -70,19 +80,15 @@ void ActiveBlockDown() {
 bool CollisionCheck() {
   //Routine to check for collision between active block and other blocks
   bool stopBlock=false;
-  //Currently broken somewhere- block collision is not yet working
   for(int i=0; i<4; i++) {
     if(boardArray[ActivePiece[i]+10]!=0) {
-      stopBlock=true;
-      for(int j=0; j<4; j++) {
-        int compareVal=ActivePiece[i]+10;
-        if(compareVal=ActivePiece[j]) {
-          stopBlock=false;
-        }
-      } 
+      if(ActivePiece[i]+10!=ActivePiece[i-1]) {
+        stopBlock=true;
+        Serial.println("Block Collision");
+      }
     }
   }
-  //Also checks for the bottom- WORKING
+  //Checks for the bottom collision
   for(int k=0; k<4; k++) {
     if(ActivePiece[k]>189) {
       stopBlock=true;
@@ -91,22 +97,56 @@ bool CollisionCheck() {
   return stopBlock;
 }
 
-void GenerateNewBlock() {
+void NewBlock() {
   //Currently generates a single I block
-  int blockChoice=1;
+  int blockChoice=random(1,5); //Replace with RandBag Generator function
   if(blockChoice==1) {
     //Generates I block
     ActivePiece[0]=35;
     ActivePiece[1]=25;
     ActivePiece[2]=15;
     ActivePiece[3]=5;
-    for(int i=0; i<4; i++) {
-      if(boardArray[ActivePiece[i]]!=0) {
-        Serial.println("Game Over");
-      }
-      else {
-        boardArray[ActivePiece[i]]=blockChoice;
-      }
+    blockCheck(blockChoice);
+  }
+  else if(blockChoice==2) {
+    ActivePiece[0]=15;
+    ActivePiece[1]=14;
+    ActivePiece[2]=5;
+    ActivePiece[3]=4;
+    blockCheck(blockChoice);
+  }
+  else if(blockChoice==3) {
+    ActivePiece[0]=13;
+    ActivePiece[1]=14;
+    ActivePiece[2]=15;
+    ActivePiece[3]=4;
+    blockCheck(blockChoice);
+  }
+  else if(blockChoice==4) {
+    ActivePiece[0]=13;
+    ActivePiece[1]=14;
+    ActivePiece[2]=4;
+    ActivePiece[3]=5;
+    blockCheck(blockChoice);
+  }
+  else if(blockChoice==5) {
+    ActivePiece[0]=16;
+    ActivePiece[1]=15;
+    ActivePiece[2]=5;
+    ActivePiece[3]=4;
+    blockCheck(blockChoice);
+  }
+}
+
+void blockCheck(int blockChoice) {
+  for(int i=0; i<4; i++) {
+    if(boardArray[ActivePiece[i]]!=0) {
+      Serial.println("Game Over");
+      pinMode(resetPin, OUTPUT); //Reset system
+      while(1);
+    }
+    else {
+      boardArray[ActivePiece[i]]=blockChoice;
     }
   }
 }
